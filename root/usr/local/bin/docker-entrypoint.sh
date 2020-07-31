444c98d3edf0b579bb82e6c15e18e16aa9fb248f
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2020-07-31 21:42:03 +0200

    manage pid + some env update

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -24,16 +24,21 @@ PLEX_MEDIA_SERVER_TMPDIR=/tmp
 LC_ALL=en_US.UTF-8
 LANG=en_US.UTF-8
 PLEX_MEDIA_SERVER_INFO_VENDOR=$(grep ^NAME= /etc/os-release | awk -F= "{print \\$2}" | tr -d \\" )
-PLEX_MEDIA_SERVER_INFO_DEVICE=PC
+PLEX_MEDIA_SERVER_INFO_DEVICE=docker
 PLEX_MEDIA_SERVER_INFO_MODEL=$(uname -m)
 PLEX_MEDIA_SERVER_INFO_PLATFORM_VERSION=$(grep ^VERSION= /etc/os-release | awk -F= "{print \\$2}" | tr -d \\" )
 LD_LIBRARY_PATH=/usr/lib/plexmediaserver/lib
+PLEX_MEDIA_SERVER_USER=${MYUSER}
 EOF
   PrepareEnvironment /tmp/plex-env
   . /etc/profile
 
   RunDropletEntrypoint
-
+  
+  if [ -e /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/plexmediaserver.pid ]; then
+    DockLog "Cleaning pid file from unclean shutdown"
+    rm -rf /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/plexmediaserver.pid
+  fi
   DockLog "Starting app: dbus-daemon"
   exec dbus-daemon --system --nofork &
   until [ -e /var/run/dbus/system_bus_socket ]; do
